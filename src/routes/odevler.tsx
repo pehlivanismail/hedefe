2e41114647f291725e3dcff1fe18d1d7c077e85e
pehlivanismail <iso.pehlivan@gmail.com>
2026-09-07 07:11:52 -0400

    fix: import DialogDescription in odevler.tsx

--- src/routes/odevler.tsx
+++ src/routes/odevler.tsx
@@ -19,6 +19,7 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
+  DialogDescription,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
